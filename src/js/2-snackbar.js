5a4a063d59a2adf30478dad3336578551cb2dd8d
Kateryna Yershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-06-05 14:05:15 +0300

    add homework

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,30 @@
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
