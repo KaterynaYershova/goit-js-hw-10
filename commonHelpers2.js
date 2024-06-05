136fe2b784f2dc417da36350545fb0ed9528f05c
KaterynaYershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-06-05 11:05:35 +0000

    Deploying to gh-pages from @ KaterynaYershova/goit-js-hw-10@5a4a063d59a2adf30478dad3336578551cb2dd8d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=parseInt(t.target.elements.delay.value),i=t.target.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{i==="fulfilled"?e(s):r(s)},s)}).then(e=>{iziToast.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{iziToast.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
